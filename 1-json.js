02b60c45d73ec31cec2a2374857f87d59e48246a
brandonkbocek <brandon.bocek@gmail.com>
2022-01-21 20:23:35 -0600

    read write json file

---
+++ 1-json.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holdiay'
+// }
+
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+
+// console.log(data.title)
+
+//read the data from the file into JS user object
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const user = JSON.parse(dataJSON)
+
+user.name = 'Brandon'
+user.age = 30
+
+//update the file with the new object
+const userJSON  = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
